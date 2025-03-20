6729fb5dfa5057f8a0c8755859d1e8458ff3321c
EngiBeerHub <engibeer0422@gmail.com>
2025-03-20 16:19:59 +0900

    Added classes

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,1 +1,18 @@
-console.log("Game Start!!");
+import rawData from "./data/questions.test.json";
+
+interface Question {
+    word: string;
+    hint: string;
+}
+
+const questions: Question[] = rawData;
+
+class Quiz {
+    questions: Question[];
+
+    constructor(questions: Question[]) {
+        this.questions = questions;
+    }
+}
+
+const quiz = new Quiz(questions);
