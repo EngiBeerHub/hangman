abbecf699d0f2afc8466da2a2bca705578b002a1
EngiBeerHub <engibeer0422@gmail.com>
2025-05-05 20:48:01 +0900

    Complete

--- src/hangman.ts
+++ src/hangman.ts
@@ -41,9 +41,6 @@ class Quiz {
     }
 }
 
-const questions: Question[] = rawData;
-const quiz = new Quiz(questions);
-
 interface UserInterface {
     input(): Promise<string>;
 
@@ -62,35 +59,6 @@ const rl = readlinePromises.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-const CLI: UserInterface = {
-    async input() {
-        const input = await rl.question("文字または単語を推測してください： ");
-        return input.replaceAll(" ", "").toLowerCase();
-    },
-    clear() {
-        console.clear();
-    },
-    destroy() {
-        rl.close();
-    },
-    output(message: string, color: Color = "white") {
-        console.log(chalk[color](message), "\n");
-    },
-    outputAnswer(message: string) {
-        console.log(figlet.textSync(message, {font: "Big"}), "\n");
-    }
-};
-
-async function testQuestion() {
-    CLI.clear();
-    const userInput = await CLI.input();
-    console.log(userInput);
-    CLI.destroy();
-}
-
-// testQuestion();
-console.log(chalk.green("正解！！"));
 
 class Stage {
     answer: string; // 解答の状態
@@ -139,3 +107,184 @@ class Stage {
         return this.leftAttempts === 0;
     }
 }
+
+class Message {
+    ui: UserInterface;
+
+    constructor(ui: UserInterface) {
+        this.ui = ui;
+    }
+
+    askQuestion(stage: Stage) {
+        this.ui.output(`Hint: ${stage.question.hint}`, "yellow");
+        this.ui.outputAnswer(stage.answer.replaceAll("", " ").trim());
+        this.ui.output(`（残りの試行回数： ${stage.leftAttempts}`);
+    }
+
+    leftQuestions(quiz: Quiz) {
+        this.ui.output(`残り${quiz.lefts() + 1}問`);
+    }
+
+    start() {
+        this.ui.output("\nGame Start!!");
+    }
+
+    enterSomething() {
+        this.ui.output(`何か文字を入力してください。`, "red");
+    }
+
+    notInclude(input: string) {
+        this.ui.output(`"${input}" は単語に含まれていません。`, "red");
+    }
+
+    notCorrect(input: string) {
+        this.ui.output(`残念！ "${input}" は正解ではありません。`, "red");
+    }
+
+    hit(input: string) {
+        this.ui.output(`"${input}" がHit!`, "green");
+    }
+
+    correct(question: Question) {
+        this.ui.output(`正解！ 単語は "${question.word}" でした。`, "green");
+    }
+
+    gameOver(question: Question) {
+        this.ui.output(`正解は ${question.word} でした。`);
+    }
+
+    end() {
+        this.ui.output("ゲーム終了です！お疲れ様でした！");
+    }
+}
+
+interface GameState {
+    stage: Stage;
+    done: boolean;
+}
+
+class Game {
+    quiz: Quiz;
+    message: Message;
+    stage: Stage;
+    ui: UserInterface;
+
+
+    constructor(quiz: Quiz, message: Message, ui: UserInterface) {
+        this.quiz = quiz;
+        this.message = message;
+        this.ui = ui;
+        this.stage = new Stage(this.quiz.getNext());
+    }
+
+    shouldEnd(): boolean {
+        if (this.stage.isGameOver()) {
+            return true;
+        }
+
+        if (!this.quiz.hasNext() && this.stage.isCorrect()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    next(isCorrect: boolean): GameState {
+        if (!isCorrect) {
+            this.stage.decrementAttempts();
+        }
+
+        if (this.shouldEnd()) {
+            return {stage: this.stage, done: true};
+        }
+
+        if (isCorrect) {
+            this.stage = new Stage(this.quiz.getNext());
+        }
+
+        return {stage: this.stage, done: false};
+    }
+
+    async start(): Promise<void> {
+        this.ui.clear();
+        this.message.start();
+
+        let state: GameState = {
+            stage: this.stage,
+            done: false
+        };
+
+        while (!state.done) {
+            if (state.stage === undefined) break;
+
+            const {stage} = state;
+
+            this.message.leftQuestions(this.quiz);
+            this.message.askQuestion(stage);
+
+            const userInput = await this.ui.input();
+
+            if (!userInput) {
+                this.message.enterSomething();
+                state = this.next(false);
+                continue;
+            }
+
+            stage.updateAnswer(userInput);
+
+            if (stage.isCorrect()) {
+                this.message.correct(stage.question);
+                state = this.next(true);
+                continue;
+            }
+
+            if (stage.isTooLong(userInput)) {
+                this.message.notCorrect(userInput);
+                state = this.next(false);
+                continue;
+            }
+
+            if (stage.isIncludes(userInput)) {
+                this.message.hit(userInput);
+                continue;
+            }
+
+            this.message.notInclude(userInput);
+            state = this.next(false);
+        }
+
+        if (state.stage.isGameOver()) {
+            this.message.gameOver(this.stage.question);
+        }
+
+        this.message.end();
+
+        this.ui.destroy();
+    }
+}
+
+const CLI: UserInterface = {
+    async input() {
+        const input = await rl.question("文字または単語を推測してください： ");
+        return input.replaceAll(" ", "").toLowerCase();
+    },
+    clear() {
+        console.clear();
+    },
+    destroy() {
+        rl.close();
+    },
+    output(message: string, color: Color = "white") {
+        console.log(chalk[color](message), "\n");
+    },
+    outputAnswer(message: string) {
+        console.log(figlet.textSync(message, {font: "Big"}), "\n");
+    }
+};
+
+const questions: Question[] = rawData;
+const quiz = new Quiz(questions);
+const message = new Message(CLI);
+const game = new Game(quiz, message, CLI);
+
+game.start();
