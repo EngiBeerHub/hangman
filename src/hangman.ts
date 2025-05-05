1b92d315927a197e8e0874cdad2eebad31cf8310
EngiBeerHub <engibeer0422@gmail.com>
2025-05-05 19:57:33 +0900

    testQuestion

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,4 +1,5 @@
 import rawData from "./data/questions.test.json";
+import readlinePromises from "readline/promises";
 
 interface Question {
     word: string;
@@ -40,3 +41,42 @@ class Quiz {
 
 const questions: Question[] = rawData;
 const quiz = new Quiz(questions);
+
+interface UserInterface {
+    input(): Promise<string>;
+
+    clear(): void;
+
+    destroy(): void;
+
+    // otput(message: string, color?: Color): void;
+    //
+    // outputAnswer(message: string): void;
+}
+
+const rl = readlinePromises.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const CLI: UserInterface = {
+    async input() {
+        const input = await rl.question("文字または単語を推測してください： ");
+        return input.replaceAll(" ", "").toLowerCase();
+    },
+    clear() {
+        console.clear();
+    },
+    destroy() {
+        rl.close();
+    }
+};
+
+async function testQuestion() {
+    CLI.clear();
+    const userInput = await CLI.input();
+    console.log(userInput);
+    CLI.destroy();
+}
+
+testQuestion();
