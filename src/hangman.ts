f6746dad30e41f521fe82be72567791be9014751
EngiBeerHub <engibeer0422@gmail.com>
2025-05-05 19:44:26 +0900

    Completed Quiz class

--- src/hangman.ts
+++ src/hangman.ts
@@ -5,14 +5,38 @@ interface Question {
     hint: string;
 }
 
-const questions: Question[] = rawData;
-
 class Quiz {
     questions: Question[];
 
     constructor(questions: Question[]) {
         this.questions = questions;
     }
+
+    /**
+     * 次の質問が存在するか確認
+     */
+    hasNext(): boolean {
+        return this.questions.length > 0;
+    }
+
+    /**
+     * ランダムに質問を取得して、その質問をリストから削除
+     */
+    getNext(): Question {
+        // 0以上、配列の長さ以下のランダムな配列を生成
+        const idx = Math.floor(Math.random() * this.questions.length);
+        // ランダムなインデックスidxを使って、questions配列から1つの問題を削除
+        const [question] = this.questions.splice(idx, 1);
+        return question;
+    }
+
+    /**
+     * 残りの質問数を取得
+     */
+    lefts(): number {
+        return this.questions.length;
+    }
 }
 
+const questions: Question[] = rawData;
 const quiz = new Quiz(questions);
